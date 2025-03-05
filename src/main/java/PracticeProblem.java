4df6f6eb76cc70204c999f23579726745dbce641
franklin-zhu-ics4u1-02-25 <123124410+FranklinZhu1@users.noreply.github.com>
2025-03-05 15:06:44 +0000

    Create solution methods

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,30 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static boolean validIndex(int[] arr, int i) {
+		try {
+			return (arr[i] == arr[i]);
+		}
+		catch (IndexOutOfBoundsException e) {
+			return false;
+		}
 	}
 
-	
+	public static int divide(int a, int b) {
+		try {
+			return a/b;
+		}
+		catch (ArithmeticException e) {
+			return 0;
+		}
+	}
+
+	public static int safeConvertStringtoInt(String s) {
+		try {
+			return Integer.parseInt(s);
+		}
+		catch (Exception e) { // too lazy to find what the specific exception is called here
+			return 0;
+		}
+	}
 
 }
